7f97bbae88e4467af410d50ad36d8925761f510d
Haikal Dwi Anggara <haikaldwianggara1708@gmail.com>
2025-09-30 12:01:46 +0700

    komit kedua

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::get('/', function () {
 
 Route::get('/fakultas', function () {
     $fakultasData = [
-        ['kode' => 'FRI', 'nama_fakultas' => 'Fakultas Rekayasa Industri'],
+        // ['kode' => 'FRI', 'nama_fakultas' => 'Fakultas Rekayasa Industri'],
         ['kode' => 'FTE', 'nama_fakultas' => 'Fakultas Teknik Elektro'],
         ['kode' => 'FIF', 'nama_fakultas' => 'Fakultas Informatika'],
         ['kode' => 'FEB', 'nama_fakultas' => 'Fakultas Ekonomi dan Bisnis'],
